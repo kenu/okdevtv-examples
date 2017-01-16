670757ad50710dac80b7529fe868b60ca5b474d1
kenu <kenu.heo@gmail.com>
2017-01-16 22:28:23 +0900

    node.js cluster, pm2

---
+++ node/cluster/single.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+}).listen(8000);
+
+console.log(`Worker ${process.pid} started`);
