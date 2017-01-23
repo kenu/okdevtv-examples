a599983eedfa4ff83cc17b2823920c67388efc62
kenu <kenu.heo@gmail.com>
2017-01-23 22:48:19 +0900

    node.js ejs template

--- node/ejs/web/routes/index.js
+++ node/ejs/web/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',
-                      name: '허광남'});
+  res.render('index', { title: 'Express2',
+                      name: '허광남2'});
 });
 
 module.exports = router;
