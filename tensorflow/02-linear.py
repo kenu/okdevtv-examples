a2e15cdf6e3409e642075cd2afaafc883e68f81a
kenu <kenu.heo@gmail.com>
2017-02-01 16:23:41 +0900

    텐서플로 첫걸음 예제 2

---
+++ tensorflow/02-linear.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+num_points = 1000
+vectors_set = []
+
+for i in xrange(num_points):
+    x1 = np.random.normal(0.0, 0.55)
+    y1 = x1 * 0.1 + 0.3 + np.random.normal(0.0, 0.03)
+    vectors_set.append([x1, y1])
+
+x_data = [v[0] for v in vectors_set]
+y_data = [v[1] for v in vectors_set]
+
+import matplotlib.pyplot as plt
+
+# plt.plot(x_data, y_data, 'ro')
+# plt.legend()
+# plt.show()
+
+
+import tensorflow as tf
+
+W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.zeros([1]))
+y = W * x_data + b
+
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in xrange(8):
+    sess.run(train)
+
+plt.plot(x_data, y_data, 'ro')
+plt.plot(x_data, sess.run(W) * x_data + sess.run(b))
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
