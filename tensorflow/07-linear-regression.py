7ed68926c3e4266415989fdaf57a12c155367f9a
kenu <kenu.heo@gmail.com>
2017-02-08 22:40:58 +0900

    logstic

--- tensorflow/07-linear-regression.py
+++ tensorflow/07-linear-regression.py
@@ -18,3 +18,24 @@ for i in range(number_of_points):
 plt.plot(x_point, y_point, 'o', label='Input Data')
 plt.legend()
 plt.show()
+
+
+A = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+B = tf.Variable(tf.zeros([1]))
+y = A * x_point + B
+cost_function = tf.reduce_mean(tf.square(y - y_point))
+optimizer = tf.train.GradientDescentOptimizer(0.5) #learning rate
+train = optimizer.minimize(cost_function)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for step in range(0, 21):
+        sess.run(train)
+        if (step % 5) == 0:
+            plt.plot(x_point, y_point, 'o',\
+                     label='step = {}'.format(step))
+            plt.plot(x_point, sess.run(A) * x_point + sess.run(B))
+            plt.legend()
+            plt.show()
