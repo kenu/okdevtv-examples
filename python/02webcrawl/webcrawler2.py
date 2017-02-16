c305ec097b16da827ceb4f0d81a2afbf99fbe0ed
kenu <kenu.heo@gmail.com>
2017-02-16 16:50:30 +0900

    python 03

--- python/02webcrawl/webcrawler2.py
+++ python/02webcrawl/webcrawler2.py
@@ -1,6 +1,6 @@
 # code from: http://creativeworks.tistory.com/178
 import requests
-import time
+from bs4 import BeautifulSoup
 
 def spider(max_pages):
     page = 1
@@ -8,8 +8,11 @@ def spider(max_pages):
         url = 'http://okjsp.tistory.com/' + str(page)
         source_code = requests.get(url)
         plain_text = source_code.text
-        print(plain_text)
-        page += 1
-#        time.sleep(30) # second
+        soup = BeautifulSoup(plain_text, 'lxml')
+        for link in soup.select('h2 > a'):
+            href = 'http://okjsp.tistory.com' + link.get('href')
+            title = link.string
+            print(href)
+            print(title)
 
 spider(2)
