5d15b3b3a470a05cc3cb394eb22024cd85a83113
kenu <kenu@okjsp.pe.kr>
2019-06-27 04:24:49 +0900

    to es5

--- babel/babel.config.js
+++ babel/babel.config.js
@@ -3,7 +3,7 @@ const presets = [
         "@babel/env",
         {
             targets: {
-                edge: "17",
+                edge: "10",
                 firefox: "60",
                 chrome: "67",
                 safari: "11.1",
