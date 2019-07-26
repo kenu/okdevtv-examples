2d6d27e081022297db96843fd7316c5547a571ac
kenu <kenu@okjsp.pe.kr>
2019-07-26 15:27:15 +0900

    docker compose added

--- docker/product/api.py
+++ docker/product/api.py
@@ -9,11 +9,12 @@ api = Api(app)
 class Product(Resource):
     def get(self):
         return {
-            'product': [
+            'products': [
                 'Ice cream',
                 'Chocolate',
                 'Fruit',
-                'Eggs'
+                'Eggs',
+                'Coffee'
             ]
         }
 
