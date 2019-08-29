8a947c87615cbb0b9e06480d4861b536f29f8f3c
kenu <kenu.heo@gmail.com>
2019-08-29 14:04:28 +0900

    !!

--- login/app.js
+++ login/app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setup!!
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
