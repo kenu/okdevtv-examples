59fe3d769eaabfd8620d401cd25c254c058492f9
kenu <kenu.heo@gmail.com>
2021-03-19 21:39:15 +0900

    node.js orm sequelize

---
+++ node/orm/orm-start.js
@@ -0,0 +1,18 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+// const sequelize = new Sequelize('sqlite::/tmp/testdb');
+
+class User extends Model {}
+User.init({
+  username: DataTypes.STRING,
+  birthday: DataTypes.DATE
+}, { sequelize, modelName: 'user' });
+
+(async () => {
+  await sequelize.sync();
+  const jane = await User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20)
+  });
+  console.log(jane.toJSON());
+})();
