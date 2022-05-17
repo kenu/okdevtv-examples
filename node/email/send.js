c7e548ed8e4b71c7ee0f9cd3d08f25825f6e6df5
kenu <kenu.heo@gmail.com>
2022-05-17 12:48:38 +0900

    nodemailer Gmail OAuth2

--- node/email/send.js
+++ node/email/send.js
@@ -2,8 +2,8 @@ const emailHelpers = require('./email');
 
 const options = {
   to: 'hbenicloud@gmail.com',
-  subject: '테스트 😭',
-  html: 'The cutest programming blog is <a href="https://www.okdevtv.com">OKdevTV.com</a>',
+  subject: '테스트1 ✅',
+  html: `The programming tv is <a href="https://okdevtv.com" target="_blank">OKdevTV.com</a>`
 };
 
 emailHelpers.sendEmail(options)
