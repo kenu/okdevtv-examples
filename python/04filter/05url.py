f72019a7fcd01a042a84f0f66b4d23971f3ec23c
kenu <kenu.heo@gmail.com>
2023-01-24 18:04:56 +0900

    key mask

--- python/04filter/05url.py
+++ python/04filter/05url.py
@@ -3,7 +3,7 @@ import requests
 import urllib
 
 def getContent(address):
-    query = "address=" + urllib.quote(address) + "&key=AIzaSyAQZD57lEciNqHdmW820QcA2mz4wrlkkyU"
+    query = "address=" + urllib.quote(address) + "&key=API-KEYAQZD57lEciNqHdmW820QcA2mz4wrlkkyU"
     return requests.get("https://maps.googleapis.com/maps/api/geocode/json?" + query).text
 
 t = getContent("경기도 연천군 군남면 삼거3길  8-17")
