8870a6b75339f8663020eccd6e22ad252292f22b
kenu <kenu.heo@gmail.com>
2023-11-05 04:57:27 +0900

    connect test

--- node/db/connect.js
+++ node/db/connect.js
@@ -1,18 +1,11 @@
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("sqlite::memory:");
 // const sequelize = new Sequelize('devdb', 'devuser', 'devpass', {
 //   host: 'localhost', dialect: 'postgres' });
 
-class User extends Model {}
-User.init(
-  { username: DataTypes.STRING, birthday: DataTypes.DATE,},
-  { sequelize, modelName: "user" }
-);
-
-(async () => {
-  await sequelize.sync();
-  const jane = await User.create({
-    username: "janedoe", birthday: new Date(1980, 6, 20),
-  });
-  console.log(jane.toJSON());
-})();
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
