44c1c1457c4676f0e0858394eb80f55041399df4
kenu <kenu.heo@gmail.com>
2024-02-29 14:26:03 +0000

    amend

--- login/app.js
+++ login/app.js
@@ -1,4 +1,4 @@
-constcreateError = require('http-errors');
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
