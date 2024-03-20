c5e22892188875c3ae737b125ad6b94dbc4eff78
kenu <kenu@okjsp.pe.kr>
2024-03-20 21:46:50 +0900

    tls

--- node/email/simplemail.js
+++ node/email/simplemail.js
@@ -1,12 +1,15 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: 'smtp.gmail.com',
   port: 587,
   secure: false, // Use `true` for port 465, `false` for all other ports
   auth: {
-    user: "kenu.heo@xmail.com",
-    pass: "**** qiym **** bhlq",  // https://myaccount.google.com/apppasswords
+    user: 'kenu.heo@xmail.com',
+    pass: 'xxxx cpxp xxxx sqha', // https://myaccount.google.com/apppasswords
+  },
+  tls: {
+    rejectUnauthorized: false, // Disables certificate validation
   },
 });
 
@@ -14,14 +17,14 @@ const transporter = nodemailer.createTransport({
 async function main() {
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: '"Kenu Heo 👻" <kenu.heo@xmail.com>', // sender address
-    to: "kenux@xkdevtv.com, heogn@xaver.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    from: '"Kenu Heo 🎉" <kenu.hex@xmail.com>', // sender address
+    to: 'kenux@okdevtv.com, heogn@xaver.com', // list of receivers
+    subject: 'Hello ✔ 메일 테스트', // Subject line
+    text: 'Hello world?', // plain text body
+    html: '<b>Hello world?</b>', // html body
   });
 
-  console.log("Message sent: %s", info.messageId);
+  console.log('Message sent: %s', info.messageId);
 }
 
 main().catch(console.error);
