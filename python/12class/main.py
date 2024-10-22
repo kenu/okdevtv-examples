62c3c7b65b485bd1ff30567d97318a61e35b75f5
kenuheo <kenuheo@okdevtv.com>
2024-10-22 13:39:56 +0900

    python class

---
+++ python/12class/main.py
@@ -0,0 +1,4 @@
+from my_math import MyMath
+
+calc = MyMath() # Create an instance of the class
+print(calc.add(1, 2))
---
+++ python/12class/my_math.py
@@ -0,0 +1,3 @@
+class MyMath:
+    def add(self, n1, n2):
+        return n1 + n2
